6704747c85210a6c9fdfde14dcea684cc60f2eab
Joël Luijmes <me@joell.dev>
2023-05-08 22:36:28 +0200

    feat(Cell): promote to class and add modifiers
    By having a reference type, we can easily change the state and all the references (e.g., the player's store) is updated as well.

--- src/main/java/dev/joell/kalaha/gamelogic/Cell.java
+++ src/main/java/dev/joell/kalaha/gamelogic/Cell.java
@@ -1,9 +1,35 @@
 package dev.joell.kalaha.gamelogic;
 
-record Cell(int numStones, boolean isStore) {
-    public Cell {
-        if (numStones < 0) {
+class Cell {
+    private final boolean isStore;
+    private int numStones;
+
+    public Cell(int initialStones, boolean isStore) {
+        if (initialStones < 0) {
             throw new IllegalArgumentException("Invalid number of stones, must be greater than 0.");
         }
+
+        this.numStones = initialStones;
+        this.isStore = isStore;
+    }
+
+    public int numStones() {
+        return numStones;
+    }
+
+    public boolean isStore() {
+        return isStore;
+    }
+
+    public void addStones(int stones) {
+        numStones += stones;
+    }
+
+    public void removeStones(int stones) {
+        if (stones > numStones) {
+            throw new IllegalArgumentException("Cannot remove more stones than there are in the cell.");
+        }
+
+        numStones -= stones;
     }
 }
--- src/test/java/dev/joell/kalaha/gamelogic/CellTests.java
+++ src/test/java/dev/joell/kalaha/gamelogic/CellTests.java
@@ -11,4 +11,26 @@ public class CellTests {
             new Cell(-1, true);
         });
     }
+
+    @Test
+    public void testRemoveStones() {
+        Cell cell = new Cell(5, false);
+        cell.removeStones(3);
+        assert cell.numStones() == 2;
+    }
+
+    @Test
+    public void testRemoveStonesInvalid() {
+        Cell cell = new Cell(5, false);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cell.removeStones(6);
+        });
+    }
+
+    @Test
+    public void testAddStones() {
+        Cell cell = new Cell(5, false);
+        cell.addStones(3);
+        assert cell.numStones() == 8;
+    }
 }

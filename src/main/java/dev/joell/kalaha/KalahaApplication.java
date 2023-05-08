a2b167f3c8b8b64c6f6b9919f2e4e10634251bcf
Joël Luijmes <me@joell.dev>
2023-05-08 23:41:36 +0200

    chore: cleanup main

--- src/main/java/dev/joell/kalaha/KalahaApplication.java
+++ src/main/java/dev/joell/kalaha/KalahaApplication.java
@@ -3,62 +3,11 @@ package dev.joell.kalaha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dev.joell.kalaha.board.Board;
-
 @SpringBootApplication
 public class KalahaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KalahaApplication.class, args);
-
-        // Board board = new Board(6, 4);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println("Index");
-        // System.out.println(board.debugBoardIndexString());
-        // System.out.println();
-        // System.out.println();
-
-        // board.makeMove(1);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(5);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(6);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(6);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(2);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(2);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(6);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
-        // board.makeMove(1);
-        // System.out.println("Points");
-        // System.out.println(board.asciiFormatString());
-        // System.out.println();
-
     }
 
 }

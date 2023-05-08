94c421365907ad93758a2ee92e99e883cdfebdaf
Joël Luijmes <me@joell.dev>
2023-05-09 00:38:53 +0200

    fix(Player): update integration test

--- src/test/java/dev/joell/kalaha/player/PlayerIntegerationIT.java
+++ src/test/java/dev/joell/kalaha/player/PlayerIntegerationIT.java
@@ -10,22 +10,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.List;
-
-import dev.joell.kalaha.player.dtos.PlayerDto;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PlayerIntegerationIT {
-    
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -33,43 +27,38 @@ public class PlayerIntegerationIT {
     @Order(1)
     public void testListPlayerEmpty() throws Exception {
         mockMvc.perform(get("/api/players"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("[]"));
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
     }
 
     @Test
     @Order(2)
     public void testCreatePlayer() throws Exception {
         mockMvc.perform(
-            post("/api/players")
-                .contentType("application/json")
-                .content("{\"name\": \"joell\"}")
-            )
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.id").value(1))
-            .andExpect(jsonPath("$.name").value("joell"));
+                post("/api/players")
+                        .contentType("application/json")
+                        .content("{\"username\": \"joell\", \"password\": \"123qwe\"}"))
+                .andExpect(status().isOk());
 
-        // TODO: not sure how to do proper test for this due the generated timestamp
     }
 
     @Test
     @Order(3)
     public void testListPlayer() throws Exception {
         mockMvc.perform(get("/api/players"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.[0].name").value("joell"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].username").value("joell"));
     }
 
     @Test
     @Order(3)
     public void testCreatePlayerRaisesOnDuplicate() throws Exception {
         mockMvc.perform(
-            post("/api/players")
-                .contentType("application/json")
-                .content("{\"name\": \"joell\"}")
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.error").value("Player with name joell already exists"));
+                post("/api/players")
+                        .contentType("application/json")
+                        .content("{\"username\": \"joell\", \"password\": \"123qwe\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Player with name joell already exists"));
 
     }
 
@@ -77,21 +66,28 @@ public class PlayerIntegerationIT {
     public void testCreatePlayerWithEmptyBody() throws Exception {
         mockMvc.perform(
                 post("/api/players")
-                    .contentType("application/json")
-            )
-            .andExpect(status().isBadRequest());
+                        .contentType("application/json"))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
     public void testCreatePlayerWithEmptyName() throws Exception {
         mockMvc.perform(
                 post("/api/players")
-                    .contentType("application/json")
-                    .content("{\"name\": \"\"}")
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.name").value( "name is missing"));
+                        .contentType("application/json")
+                        .content("{\"username\": \"\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.username").value("name is missing"));
+    }
+
+    @Test
+    public void testCreatePlayerWithWeakPassword() throws Exception {
+        mockMvc.perform(
+                post("/api/players")
+                        .contentType("application/json")
+                        .content("{\"username\": \"jan\", \"password\": \"123\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.password").value("password must be at least 6 characters"));
     }
 
 }
-

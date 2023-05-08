c6505d7071cf8bfce6a9414cce0fb4fd39044e34
Joël Luijmes <me@joell.dev>
2023-05-08 21:30:37 +0200

    feat(GameState): implement toString()

--- src/main/java/dev/joell/kalaha/gamelogic/GameState.java
+++ src/main/java/dev/joell/kalaha/gamelogic/GameState.java
@@ -24,4 +24,61 @@ public record GameState(
         }
     }
 
+    /**
+     * Format the board as a pretty human readable string.
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Current player: %s\n\n", currentPlayer.name()));
+
+        int len = board.length / 2;
+
+        // Draw the index row, which indicates the index of each cup for player A
+        sb.append("   A  ");
+        for (int i = 1; i < len; i++) {
+            sb.append(String.format("   %s  ", i));
+        }
+        sb.append("   B  \n");
+
+        // Draw separator line
+        for (int i = 0; i <= len; i++) {
+            sb.append("+-----");
+        }
+        sb.append("+\n");
+
+        // Draw the cup row for player A
+        sb.append("|      ");
+        for (Cell cell : playerA.cups()) {
+            sb.append(String.format(" %2d  |", cell.numStones()));
+        }
+        sb.append("     |\n");
+
+        // Draw the store value for player A
+        sb.append(String.format("| %2d  ", playerA.store().numStones()));
+
+        // Draw the separator line between the two cup rows
+        for (int i = 0; i < len - 1; i++) {
+            sb.append("+-----");
+        }
+
+        // Draw the store value for player B
+        sb.append(String.format("+ %2d  ", playerB.store().numStones()));
+        sb.append("+\n");
+
+        // Draw the cup row for player B
+        sb.append("|     ");
+        for (Cell cell : playerB.cups()) {
+            sb.append(String.format("| %2d  ", cell.numStones()));
+        }
+        sb.append("      |\n");
+
+        // Draw the separator line
+        for (int i = 0; i < len + 1; i++) {
+            sb.append("+-----");
+        }
+        sb.append("+\n");
+
+        return sb.toString();
+    }
+
 }
--- src/test/java/dev/joell/kalaha/gamelogic/GameStateTests.java
+++ src/test/java/dev/joell/kalaha/gamelogic/GameStateTests.java
@@ -1,5 +1,6 @@
 package dev.joell.kalaha.gamelogic;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -57,5 +58,42 @@ public class GameStateTests {
         assertThrows(IllegalArgumentException.class, () -> {
             new GameState(board, playerA, playerB, currentPlayer);
         });
+    }
+
+    @Test
+    public void formatToString() {
+        // Random fake initialization of the game state. This is just to test the
+        // toString() method.
+        // Testing initialization of the game state is done in GameLogicTests.
+        Cell[] board = new Cell[14];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = new Cell(4, false);
+        }
+
+        Cell[] playerACups = new Cell[6];
+        for (int i = 0; i < playerACups.length; i++) {
+            playerACups[i] = new Cell(i + 1, false);
+        }
+        PlayerState playerA = new PlayerState(playerACups, new Cell(6, true), "Player A");
+
+        Cell[] playerBCups = new Cell[6];
+        for (int i = 0; i < playerBCups.length; i++) {
+            playerBCups[i] = new Cell(6 - i, false);
+        }
+        PlayerState playerB = new PlayerState(playerBCups, new Cell(9, true), "Player B");
+
+        GameState game = new GameState(board, playerA, playerB, playerA);
+
+        String expected = "" +
+                "Current player: Player A\n" +
+                "\n" +
+                "   A     1     2     3     4     5     6     B  \n" +
+                "+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+                "|        1  |  2  |  3  |  4  |  5  |  6  |     |\n" +
+                "|  6  +-----+-----+-----+-----+-----+-----+  9  +\n" +
+                "|     |  6  |  5  |  4  |  3  |  2  |  1        |\n" +
+                "+-----+-----+-----+-----+-----+-----+-----+-----+\n";
+
+        assertEquals(expected, game.toString());
     }
 }

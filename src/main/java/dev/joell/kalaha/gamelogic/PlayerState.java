170ba6d2e0802bfe7f0ecd757263991e75fd0360
Joël Luijmes <me@joell.dev>
2023-05-08 21:34:21 +0200

    feat(PlayerState): determine whether player has any stones left

--- src/main/java/dev/joell/kalaha/gamelogic/PlayerState.java
+++ src/main/java/dev/joell/kalaha/gamelogic/PlayerState.java
@@ -12,4 +12,17 @@ public record PlayerState(Cell[] cups, Cell store, String name) {
             throw new IllegalArgumentException("Name cannot be null.");
         }
     }
+
+    /**
+     * Check if the player has any stones in any of their cups.
+     */
+    public boolean hasStonesInAnyCup() {
+        for (Cell cup : cups) {
+            if (cup.numStones() > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/test/java/dev/joell/kalaha/gamelogic/PlayerStateTests.java
+++ src/test/java/dev/joell/kalaha/gamelogic/PlayerStateTests.java
@@ -1,6 +1,8 @@
 package dev.joell.kalaha.gamelogic;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,4 +27,23 @@ public class PlayerStateTests {
             new PlayerState(new Cell[1], new Cell(0, true), null);
         });
     }
+
+    @Test
+    public void hasStonesInAnyCupWithStones() {
+        PlayerState player = new PlayerState(new Cell[] {
+                new Cell(1, false),
+        }, new Cell(0, true), "Player A");
+
+        assertTrue(player.hasStonesInAnyCup());
+    }
+
+    @Test
+    public void hasStonesInAnyCupWithNoStones() {
+        PlayerState player = new PlayerState(new Cell[] {
+                new Cell(0, false),
+        }, new Cell(0, true), "Player A");
+
+        assertFalse(player.hasStonesInAnyCup());
+    }
+
 }
